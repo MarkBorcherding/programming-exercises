e5dec623918c9ced2b42f4edf102258c645ae514
Mark Borcherding <markborcherding@gmail.com>
2014-12-05 22:43:11 -0600

    iterative

---
+++ make-change/iterative.rb
@@ -0,0 +1,15 @@
+
+DENOMINATIONS = [1, 5, 10, 25]
+
+def make_change(amount)
+  change = {}
+  DENOMINATIONS.sort.reverse.each do |d|
+    change[d] = amount / d
+    amount = amount % d
+  end
+  change
+end
+
+puts make_change(2)
+puts make_change(20)
+puts make_change(27)
