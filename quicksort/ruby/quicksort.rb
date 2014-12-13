59d7a31d6831f4f1385de50db2bee308a9199fe6
Mark Borcherding <markborcherding@gmail.com>
2014-12-13 10:14:47 -0600

    tweak ruby quicksort a bit

--- quicksort/ruby/quicksort.rb
+++ quicksort/ruby/quicksort.rb
@@ -1,10 +1,14 @@
 def qs(l)
-  return [] if l == []
-  head = qs(l[1..-1].reject { |i| i >= l[0] })
-  split = [l[0]]
-  tail = qs(l[1..-1].reject { |i| i < l[0] })
-  head + split + tail
+  if l == []
+    []
+  else
+    pivot, *tail = l
+    qs(tail.reject { |i| i >= pivot }) +
+      [pivot] +
+      qs(tail.reject { |i| i < pivot })
+  end
 end
 
 puts qs([1, 2, 3, 4, 5, 6]).inspect
 puts qs([6, 4, 5, 2, 1, 3]).inspect
+puts qs([6, 4, 5, 7, 5, 2, 1, 3]).inspect
