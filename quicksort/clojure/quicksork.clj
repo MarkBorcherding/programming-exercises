acb71e4e42d980094fe9c8bb287751c0bc900707
Mark Borcherding <markborcherding@gmail.com>
2014-12-15 09:36:12 -0600

    fixed

--- quicksort/clojure/quicksork.clj
+++ quicksort/clojure/quicksork.clj
@@ -1,10 +1,9 @@
-; not working yet
 (defn quicksort [l]
   (if (empty? l)
     '()
     (let [[pivot & tail] l]
           (lazy-cat (quicksort (filter (fn [x] (< x pivot)) tail))
                     (list pivot)
-                    (quicksort (filter (fn [x] (>= pivot x)) tail))))))
+                    (quicksort (remove (fn [x] (< x pivot)) tail))))))
 
 (println (quicksort '(5 1 4 2 3)))
