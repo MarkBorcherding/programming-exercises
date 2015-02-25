9fba999da2d24c9501fdc4cfd97977ba679dd9c8
Mark Borcherding <markborcherding@gmail.com>
2015-02-24 22:37:22 -0600

    do not let quality be negative

--- guilded-rose/clojure/guilded-rose/src/guilded_rose/core.clj
+++ guilded-rose/clojure/guilded-rose/src/guilded_rose/core.clj
@@ -2,11 +2,6 @@
 
 (defn update-quality [name sell-in quality]
   (let [sell-in (- sell-in 1)
-        quality (- quality 1)]
+        quality (max 0 (- quality (if (< 0 sell-in) 1 2)))]
         [name sell-in quality]))
-
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
 
--- guilded-rose/clojure/guilded-rose/test/guilded_rose/core_test.clj
+++ guilded-rose/clojure/guilded-rose/test/guilded_rose/core_test.clj
@@ -3,7 +3,20 @@
             [guilded-rose.core :refer :all]))
 
 (deftest a-test
-  (testing "updating a quality on a normal item"
-    (is (= ( update-quality :normal-item 2 3 )
-           [:normal-item 1 2]))))
+  (testing "update quality"
+
+    (testing "normal items"
+      (is (= (update-quality :normal-item 2 3)
+             [:normal-item 1 2])
+          "Items degrade daily")
+
+      (is (= (update-quality :normal-item 0 3)
+             [:normal-item -1 1])
+          "Expired items degrade twice as fast")
+
+      (is (= (update-quality :normal-item 0 0)
+             [:normal-item -1 0])
+          "Quality can never be negative")
+
+      )))
 
