ddcb53aa35a38ddfe50c09119873c071ded135dc
Mark Borcherding <markborcherding@gmail.com>
2015-02-26 18:11:52 -0600

    trying multi dispatch methods

--- guilded-rose/clojure/guilded-rose/src/guilded_rose/core.clj
+++ guilded-rose/clojure/guilded-rose/src/guilded_rose/core.clj
@@ -1,7 +1,19 @@
 (ns guilded-rose.core)
 
-(defn update-quality [name sell-in quality]
-  (let [sell-in (- sell-in 1)
-        quality (max 0 (- quality (if (< 0 sell-in) 1 2)))]
-        [name sell-in quality]))
+(defn item [name sell-in quality]
+      {:name name
+       :sell-in sell-in,
+       :quality quality})
+
+(defn lower-sell-in [item] (item (:name item)
+                                 (- (:sell-in item) 1)
+                                 (:quality item)))
+
+
+(defmulti update-quality #((:item %)))
+(defmethod update-quality :normal-item [x]
+  (let [x (lower-sell-in x)]
+    (item (:name x)
+          (:sell-in x)
+          (max 0 (- (:quality x) (if (< 0 (:sell-in x)) 1 2))))))
 
--- guilded-rose/clojure/guilded-rose/test/guilded_rose/core_test.clj
+++ guilded-rose/clojure/guilded-rose/test/guilded_rose/core_test.clj
@@ -6,16 +6,16 @@
   (testing "update quality"
 
     (testing "normal items"
-      (is (= (update-quality :normal-item 2 3)
-             [:normal-item 1 2])
+      (is (= (update-quality (item :normal-item 2 3))
+             (item :normal-item 1 2))
           "Items degrade daily")
 
-      (is (= (update-quality :normal-item 0 3)
-             [:normal-item -1 1])
+      (is (= (update-quality (item :normal-item 0 3))
+             (item :normal-item -1 1))
           "Expired items degrade twice as fast")
 
-      (is (= (update-quality :normal-item 0 0)
-             [:normal-item -1 0])
+      (is (= (update-quality (item :normal-item 0 0))
+             (item :normal-item -1 0))
           "Quality can never be negative")
 
       )))
